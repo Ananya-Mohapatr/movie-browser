ad769b8ab7d028cc23e3142b5bbe6c75432bdd99
Ananya Mohapatra <ananyamohapatra27@gmail.com>
2024-08-14 12:21:28 +0530

    Added images and folder for rough sketch

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+import bgImg from '../assets/background-image.png'
+const Header = () => {
+  return (
+    <div>
+    <div className='header'>
+Header
+
+    </div>
+    <img src={bgImg}/>
+    </div>
+  )
+}
+
+export default Header
