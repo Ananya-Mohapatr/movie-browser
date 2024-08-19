9b07da585242672f5ddf6372099f78503e54be75
Ananya Mohapatra <ananyamohapatra27@gmail.com>
2024-08-19 08:10:18 +0530

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,14 @@ import React,{useState,useEffect} from 'react'
 import {ACCOUNT_ID,API_READ_ACCESS_TOKEN,API_URL} from '../utils/Constant'
 import './Header.css'
 import bgImg from '../assets/background-image.png'
-import fav from '../assets/bookmark_13484736.png'
+import notBookmarked from '../assets/bookmark_2107915.png'
+import bookmarked from '../assets/bookmark_2107725.png'
 import ReadMorePopUp from '../modals/ReadMorePopUp'
 const Header = () => {
   const [movieList,setMovieList] = useState([])
   const [open,setOpen] = useState(false)
   const [overview,setOverview] = useState("")
   const fetchMovieList = async()=>{
-    let account_id = ACCOUNT_ID
-    let res = 
     await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_READ_ACCESS_TOKEN}`,{
             method:'GET',
       headers: {
@@ -23,7 +22,12 @@ const Header = () => {
     .then(response=>response.json())
     .then(response=>{
       console.log("response",response)
-      setMovieList(response.results)
+      let newRes = response.results.map((i)=>{
+        i.bookmarked = false
+        i.bookmarkedImg = notBookmarked
+        return i 
+      })
+      setMovieList(newRes)
     })
     .catch(err=>console.log("error",err))
   }
@@ -36,22 +40,31 @@ const Header = () => {
   const closePopup=()=>{
    setOpen(false)
   }
+  const handleBookmark = (id)=>{
+    let newList = movieList.map((i)=>{
+      if(i.id === id ){
+        i.bookmarked = !i.bookmarked
+        i.bookmarkedImg = i.bookmarked ? bookmarked : notBookmarked
+      }
+      return i
+      
+    })
+    console.log("newList",newList)
+    setMovieList(newList)
+
+  }
   useEffect(()=>{
     fetchMovieList()
   },[])
   return (
     <div>
-    <div className='header'>
-Header
-
-    </div>
     <div style={{display:'grid',gridTemplateColumns:'auto auto auto auto'}} >
     {/* <img src={bgImg} className='bgImgg'/> */}
     {movieList.map((i)=>{
       return (<div className='card' >
-        <div><b>{i.title || i.name} ({i?.release_date?.toString().slice(0,4) || i?.first_air_date?.toString().slice(0,4)})</b><img src={fav} style={{height:'25px',float:'right'}}/></div>
-        <img src={'https://image.tmdb.org/t/p/w220_and_h330_face'+i.poster_path} className='img'/>
-        <p>{i.overview.length<50 ?i.overview : i.overview.substr(0,80)+"..."}</p>{i.overview.length>80?<button style={{'color':'blue'}} onClick={()=>openPopup(i.overview)} >Read More</button>:''}
+        <div><b>{i?.name || i?.title } ({i?.release_date?.toString().slice(0,4) || i?.first_air_date?.toString().slice(0,4)})</b><img onClick={()=>handleBookmark(i?.id)}src={i?.bookmarkedImg} style={{height:'25px',float:'right'}}/></div>
+        <img src={'https://image.tmdb.org/t/p/w220_and_h330_face'+i?.poster_path} className='img'/>
+        <p>{i?.overview?.length<50 ?i?.overview : i?.overview.substr(0,80)+"..."}</p>{i?.overview?.length>80?<button style={{'color':'blue'}} onClick={()=>openPopup(i?.overview)} >Read More</button>:''}
         </div>)
     })}
     </div>
--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './Header'
-
+import './Header.css'
 const MainBody = () => {
   return (
     <div>
+      <div className='header'>
+Header
+
+    </div>
         <Header/>
     </div>
   )
